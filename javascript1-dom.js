a8a62a9c3ae6e8eff58f8884f34be804e498edc1
tagrag90 <gunjeepark21@gmail.com>
2020-06-25 20:50:01 +0900

    계산기 작동 js

---
+++ javascript1-dom.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function () {
+  var txtX = document.getElementById("txt-x");
+  var txtY = document.getElementById("txt-y");
+  var btnAdd = document.getElementById("btn-add");
+  var txtSum = document.getElementById("txt-sum");
+
+  btnAdd.onclick = function () {
+    // console.log("ad");
+    var x = parseInt(txtX.value);
+    var y = parseInt(txtY.value);
+
+    txtSum.value = x + y;
+  };
+});
